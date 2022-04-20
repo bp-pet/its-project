6bf6494905c96dc800b07a30f87a1299b877a87d
s2020343 <s2020343@utwente.nl>
2022-04-20 17:51:43 +0200

    second version

--- lempelziv.py
+++ lempelziv.py
@@ -1,68 +1,98 @@
-input_string = "Hello everyone! Hello world!"
+"""
+Module for performing sliding window Lempel-Ziv on a string.
+"""
 
-code_string = ""
+from typing import List, Tuple, Union
 
-window_size = 25
-buffer_size = 8
-lookback_size = window_size - buffer_size
+# set parameters
+# source = "Hello everyone! Hello world!"
+with open('source.txt') as f:
+    source = f.read()
+window_size = 50
+buffer_size = 10
+verbose = False
 
+def encode(input_string: str) -> List[Union[str, Tuple[int]]]:
 
-i = 0 # can't use for loop because we want to skip multiple iterations sometimes
-while True:
-    if i == len(input_string):
-        # break after string is done
-        break
-
-    # get the lookback and buffer strings
-    lookback = input_string[max(0, i - lookback_size):i]
-    buffer = input_string[i:min(i + buffer_size, len(input_string))]
-
-    # find a match for the first character in the buffer
-    starting_index = None
-    for ind, j in enumerate(lookback):
-        # find first instance of first buffer item in lookback window
-        if buffer[0] == j:
-            starting_index = ind
+    result = []
+    lookback_size = window_size - buffer_size
+    marker = 0 # marks the start of the buffer
+    while True:
+        if verbose:
+            print("---------------------------------------------Current start of buffer", marker)
+        if marker == len(input_string):
+            # break after string is done
             break
-    if starting_index is None:
-        # if no matches found then encode string as itself and continue
-        code_string += buffer[0]
-        i += 1
-        continue
 
-    # find the end of the match
-    match_length = 1
-    for ind, j in enumerate(buffer[1:]):
-        # check how far in the buffer we can go before match ends
-        if starting_index + ind + 1 > len(lookback) - 1:
-            # reached end of lookback window
-            break
-        if j == lookback[starting_index + ind + 1]:
-            # if next character is match, keep looking
-            match_length += 1
+        # get the lookback and buffer strings
+        lookback = input_string[max(0, marker - lookback_size):marker]
+        buffer = input_string[marker:min(marker + buffer_size, len(input_string))]
+        if verbose:
+            print(lookback + "|" + buffer)
+
+        offset = 1
+        match_length = 0
+
+        current_offset = 1
+        for current_offset in range(1, len(lookback) + 1): # stop when beginning of lookback window is reached
+            if verbose:
+                print("-------------current offset", current_offset)
+            current_length = 0
+            while True:
+                # for this offset, find the longest match
+                if verbose:
+                    print("current length", current_length)
+                if (lookback + buffer)[len(lookback) - current_offset + current_length] == buffer[current_length]:
+                    # check if next character matches the next character of the buffer
+                    current_length += 1
+                else:
+                    # if not then stop looking
+                    break
+                if current_length >= len(buffer):
+                    # also stop looking if match is as long as the buffer
+                    break
+            if current_length > match_length:
+                # if this is the longest match so far, record it
+                match_length = current_length
+                offset = current_offset
+
+
+
+        # encode match
+        if match_length < 2:
+            # no special encoding if match is length 1
+            result.append(buffer[0])
+            marker += 1
         else:
-            # if not match, we stop looking
-            break
-
-    # encode match
-    if match_length == 1:
-        # no special encoding if match is length 1
-        code_string += buffer[0]
-        i += 1
-    else:
-        # else write encoding and jump forward
-        code_string += f"<{lookback_size - starting_index - 1}, {match_length}>"
-        i += match_length
+            # else write encoding and jump forward
+            result.append((offset, match_length))
+            marker += match_length
+    return result
 
 
-print(code_string)
+def decode(code: List[Union[str, Tuple[int]]]) -> str:
+    result = ""
+    for c in code:
+        if isinstance(c, str):
+            result += c
+        else:
+            offset, length = c[0], c[1]
+            if offset > len(result):
+                raise Exception("Invalid code, offset is out of bounds")
 
-# TODO change search technique: first maximize L, then minimize m (take rightmost longest match)
-# TODO change it so that match can overlap with buffer
+            start = len(result) - offset
+            for i in range(length):
+                result += result[start + i]
+    return result
 
-# Paul will look into how to actually encode (look into paper)
 
-# Dimitri will write out exercises
+if verbose:
+    print("Source:", source)
+encoded = encode(source)
+if verbose:
+    print("Encoded:", encoded)
+decoded = decode(encoded)
+if verbose:
+    print("Decoded:", decoded)
 
-# research questions:
-# prove that if window size becomes large we approach the entropy
+print(source == decoded)
